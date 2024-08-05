478c169a407e0563cc76bccdc11c198e000e0fa4
irena binnebesel <irenabinnebesel1@gmail.com>
2024-08-05 12:18:50 +0100

    testing

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -12,7 +12,7 @@
  * calculate('/', 121, 11); // should return 11
  * calculate('sq', 5); // should return 25
  * calculate('sq', 3, 44); // should return 9, as the 44 is ignored
- * calculate('%', 124, 56); // should throw an error since '%' is not a supported operator
+ * calculate('%', 124, 56); // should throw an errorsince '%' is not a supported operator
  * calculate('🍕', -41414, 2); // should throw an error since '🍕' is not a supported operator
  * ```
  */
---
+++ calculator/calculator.test.js
@@ -0,0 +1,7 @@
+import {test, expect } from 'vitest';
+import {calculate} from './calculator';
+
+test ('addition of 1 and 2 should return 3', ()=> {
+    expect(calculate('-',1,2 )).toBe (-1)
+})
+
